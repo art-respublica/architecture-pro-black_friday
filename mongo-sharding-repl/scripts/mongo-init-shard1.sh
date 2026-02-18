64281aa44a8191f46434344826ccb8499d956aba
Артем Дяченко <ADyachenko3@alfabank.ru>
2026-02-18 21:45:08 +0300

    Задание 3, выполнено

--- mongo-sharding-repl/scripts/mongo-init-shard1.sh
+++ mongo-sharding-repl/scripts/mongo-init-shard1.sh
@@ -1,7 +1,9 @@
-docker exec -it mongo-sharding-shard1 mongosh --port 27017 --eval '
+docker exec -it mongo-sharding-shard1-1 mongosh --port 27017 --eval '
 rs.initiate({
   _id: "shard1ReplSet",
   members: [
-    { _id: 0, host: "mongo-sharding-shard1:27017" }
+    { _id: 0, host: "mongo-sharding-shard1-1:27017" },
+    { _id: 1, host: "mongo-sharding-shard1-2:27017" },
+    { _id: 2, host: "mongo-sharding-shard1-3:27017" }
   ]
 })'
--- mongo-sharding-repl/scripts/mongo-init-shard2.sh
+++ mongo-sharding-repl/scripts/mongo-init-shard2.sh
@@ -1,7 +1,9 @@
-docker exec -it mongo-sharding-shard2 mongosh --port 27017 --eval '
+docker exec -it mongo-sharding-shard2-1 mongosh --port 27017 --eval '
 rs.initiate({
   _id: "shard2ReplSet",
   members: [
-    { _id: 0, host: "mongo-sharding-shard2:27017" }
+    { _id: 0, host: "mongo-sharding-shard2-1:27017" },
+    { _id: 1, host: "mongo-sharding-shard2-2:27017" },
+    { _id: 2, host: "mongo-sharding-shard2-3:27017" }
   ]
 })'
